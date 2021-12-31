f268aacb77d1522a489f432860756918636047cf
Faisal Akhtar <faisal.akhtar@tecsys.com>
2021-12-31 15:48:09 -0500

    sample changes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,16 @@
 <template>
   <v-container>
     <v-row class="text-center">
+      {{textsample}}
 
+    </v-row>
+        <v-row class="text-center">
+
+                <v-text-field
+            label="Name"
+            outlined
+            v-model.lazy="textsample"
+          ></v-text-field>
     </v-row>
   </v-container>
 </template>
@@ -11,7 +20,7 @@
     name: 'HelloWorld',
 
     data: () => ({
-
+        textsample:"",
         counter: 1
     }),
   }
