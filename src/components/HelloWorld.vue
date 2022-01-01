35ae1a8e167acdc559aca279a7f8370e07f17467
Faisal Akhtar <faisal.akhtar@tecsys.com>
2022-01-01 14:52:43 -0500

    Adding Notes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,21 +3,65 @@
 
 
       <v-container>
-        <v-card 
-        max-width="60%"
-        >          
-          <v-card-title>
-            <h2>Customer Details</h2> <v-spacer></v-spacer> <v-btn @click.stop="drawer = !drawer">Edit</v-btn>
-          </v-card-title>
-          
-          <v-card-text>
-            <v-row style="padding:1%">
-            <ContactInformation :contactInfo="contactInformation"/>
-            <v-spacer></v-spacer>
-            <AddtionalInfo />
-            </v-row>
-          </v-card-text>  
-        </v-card>
+        <v-row>
+        <v-col 
+        cols="8"
+        >
+          <v-card 
+          >          
+            <v-card-title>
+              Customer Details<v-spacer></v-spacer> <v-btn flat  @click.stop="drawer = !drawer">Edit</v-btn>
+            </v-card-title>
+            
+            <v-card-text>
+              <v-row style="padding:1%">
+              <ContactInformation :contactInfo="contactInformation"/>
+              <v-spacer></v-spacer>
+              <AddtionalInfo />
+              </v-row>
+            </v-card-text>  
+          </v-card>
+        </v-col>
+        <v-col
+        cols="4">
+                            <v-card 
+          >          
+            <v-card-title>
+              Notes ({{notes.length}} notes)
+            </v-card-title>
+            
+            <v-card-text>
+               <v-btn block @click.stop="notedrawer = !notedrawer">Add new note</v-btn>
+              <v-row v-for="(item,index) in notes" :key="index" style="padding:1%">
+                <v-container>
+                  <v-card
+                  flat
+                  outlined>
+                  <v-card-title>
+                        {{item.Name}}
+                    </v-card-title>
+                  <v-card-text>
+                  <v-row style="padding:1%">
+                    <v-col>
+                      <v-row>
+                      {{item.Date}}
+                      </v-row>
+                      <v-row>
+                      {{item.Note}}
+                      </v-row>
+                      </v-col>  
+                  </v-row>
+            </v-card-text>
+                  </v-card>
+                </v-container>
+              </v-row>
+            </v-card-text>  
+          </v-card>
+        </v-col>
+        </v-row>
+        <NoteDrawer v-model="notedrawer" 
+        @savenoteinfo="savenote"
+         />
         <CustomDrawer v-model="drawer" 
         :contactInfo="contactInformation"
         @savecontactinfo="savecontact"
@@ -31,13 +75,15 @@
 import ContactInformation from './ContactInformation.vue'
 import AddtionalInfo from './AddtionalInfo.vue'
 import CustomDrawer from './CustomDrawer.vue'
+import NoteDrawer from './NoteDrawer.vue'
 
   export default {
     name: 'HelloWorld',
     components: {
       ContactInformation,
       AddtionalInfo,
-      CustomDrawer
+      CustomDrawer,
+      NoteDrawer
     },
     methods:
     {
@@ -46,18 +92,28 @@ import CustomDrawer from './CustomDrawer.vue'
         this.contactInformation.Email = data.Email
         this.contactInformation.Address = data.Address
         this.contactInformation.Language = data.Language
+      },
+      savenote: function(note)
+      {
+        this.notes.push({
+          Name: note.Name,
+          Note:note.Note,
+          Date: note.CurrentDate
+        })
       }
     },
     data: () => ({
         textsample:"", 
         counter: 1,
         drawer: false,
+        notedrawer: false,
         contactInformation : 
         {
           Email: "faisal.test@email.com",
           Address: "1500 Allstate Pkwy, Markham, ON L3R 5B4",
           Language: "English"
-        }
+        },
+        notes: []
     }),
   }
 </script>
---
+++ src/components/NoteDrawer.vue
@@ -0,0 +1,89 @@
+   
+<template>
+      <v-navigation-drawer
+      v-model="curDrawer"
+      absolute
+      bottom
+      temporary
+      right
+      width="25%"
+    >
+          <v-list
+        
+        dense
+      >
+
+          <v-list-item>
+         <v-text-field
+            label="Name"
+            outlined
+            v-model="name"
+          ></v-text-field>
+          </v-list-item>
+        <v-list-item>
+         <v-textarea
+            label="Note"
+            outlined
+            v-model="note"
+          ></v-textarea>
+          </v-list-item>
+        <v-list-item>
+          </v-list-item>
+
+        <v-list-item>
+            <v-btn block @click="SaveNoteInfo">Save</v-btn>
+          </v-list-item>
+
+      </v-list>
+    </v-navigation-drawer>
+</template>
+
+
+
+<script>
+export default {
+name: "NoteDrawer",
+props : ["drawer",],
+model: {
+  prop:'drawer',
+  event: 'drawerChange'
+},
+data: function()
+{
+  return{
+    group:null,
+    name: "",
+    note: ""
+  }
+},
+computed : {
+  curDrawer : {
+    get: function()
+    {
+      return this.drawer
+    },
+    set : function(value)
+    {
+      this.$emit('drawerChange',value);
+    }
+  },
+},
+methods:{
+    SaveNoteInfo: function()
+    {
+        this.$emit('savenoteinfo',
+        {
+            Name: this.name,
+            Note : this.note,
+            CurrentDate: new Date().toLocaleString().replace(',','')
+        });
+        this.curDrawer = false;
+
+    }
+
+}
+}
+</script>
+
+<style>
+</style>
